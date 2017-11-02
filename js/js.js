10a47d345e4a02edfe9b9c6cecfca787bb98c46d
mollyyee <mollyyee1@Mollys-MacBook-Pro-3.local>
2017-11-01 17:42:29 -0700

    new version

---
+++ js/js.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+
+	$(function() {
+	  $("#1").delay(50).fadeIn();
+	});
+	$(function() {
+	  $("#2").delay(100).fadeIn();
+	});
+	$(function() {
+	  $("#3").delay(150).fadeIn();
+	});
+	$(function() {
+	  $("#4").delay(200).fadeIn();
+	});
+	$(function() {
+	  $("#5").delay(250).fadeIn();
+	});
+	$(function() {
+	  $("#6").delay(300).fadeIn();
+	});
+	$(function() {
+	  $("#7").delay(350).fadeIn();
+	});
+	$(function() {
+	  $("#8").delay(400).fadeIn();
+	});
+	$(function() {
+	  $("#9").delay(400).fadeIn();
+	});
+	$(function() {
+	  $("#10").delay(400).fadeIn();
+	});
+	$(function() {
+	  $("#11").delay(400).fadeIn();
+	});
+
+	$(function() {
+	  $("#12").delay(50).fadeIn();
+	});
+	$(function() {
+	  $("#13").delay(100).fadeIn();
+	});
+	$(function() {
+	  $("#14").delay(150).fadeIn();
+	});
+	$(function() {
+	  $("#15").delay(200).fadeIn();
+	});
+	$(function() {
+	  $("#16").delay(250).fadeIn();
+	});
+	$(function() {
+	  $("#17").delay(300).fadeIn();
+	});
+	$(function() {
+	  $("#18").delay(350).fadeIn();
+	});
+	$(function() {
+	  $("#19").delay(400).fadeIn();
+	});
+	$(function() {
+	  $("#20").delay(400).fadeIn();
+	});
+	$(function() {
+	  $("#21").delay(400).fadeIn();
+	});
+	$(function() {
+	  $("#22").delay(400).fadeIn();
+	});
+
+
+
+	// $(function() {
+	//   $(".navbar").delay(800).fadeIn();
+	// });
+
+
+	$(function() {
+	  $(".fadein").delay(800).fadeIn();
+	});
+
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 300) {
+    $('.navbar').fadeIn();
+  } 
+  // else {
+  //   $('.navbar').fadeOut();
+  // }
+});
+
+
+})
